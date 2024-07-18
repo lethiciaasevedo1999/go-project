3963290a618922dfd180843bb5117165eba88f2f
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-18 00:17:17 -0300

    Utilizando função para verificar o tipo de variável, e diferentes formas de declarações de variáveis

--- Hello.go
+++ Hello.go
@@ -1,11 +1,14 @@
 package main
 
 import "fmt"
+import "reflect" // pacote utilizado para saber qual é o tipo da variável utilizada
 
 func main() {
-	var nome string = "Lethicia"
-	var idade int = 25
-	var versao  float32 = 1.1
+	var nome = "Lethicia" // ou nome := “Lethicia” 
+	var idade  = 25 // ou idade := 25
+	var versao  = 1.1 // ou versao := 1.2
 	fmt.Println("Olá",nome, "sua idade é:", idade, "anos")
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variável nome é:", reflect.TypeOf(nome))
 }
