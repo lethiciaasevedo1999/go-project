063aaa856a65b9dfcbe18ba5b3092b6d7c3c742d
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-18 00:03:22 -0300

    Tipos de variáveis

--- Hello.go
+++ Hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World");
+	var nome string = "Lethicia"
+	var idade int = 25
+	var versao  float32 = 1.1
+	fmt.Println("Olá",nome, "sua idade é:", idade, "anos")
+	fmt.Println("Este programa está na versão", versao)
 }
