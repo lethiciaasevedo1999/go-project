59a048df595167945166b4660cac4c4f81b632ae
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-19 01:04:57 -0300

    Implementando if e elses

--- Hello.go
+++ Hello.go
@@ -14,8 +14,19 @@ func main() {
 
 	var comando int
 	fmt.Scan(&comando) 
-	fmt.Println("O endereço da minha varia'vel comando é", &comando)
 	fmt.Println("O comando escolhido foi", comando)
 	//Scanf é a função para receber dados do usuário 
 	//& - serve para indicar que vai salvar na variável oque o usuário digitar
+
+
+	if comando == 1 {
+		fmt.Println("Monitorando...")
+	}else if comando == 2{
+		fmt.Println("Exibindo Logs...")
+	}else if comando == 0{
+		fmt.Println("Saindo do programa...")
+	}else{
+		fmt.Println("Não conheço este comando")
+	}
+
 }
