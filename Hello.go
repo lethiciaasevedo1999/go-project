6d12ec307d5f1eab575d83048c1eb1eaefd78f42
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-24 00:34:00 -0300

    Colocando o programa em estado de looping com for

--- Hello.go
+++ Hello.go
@@ -7,64 +7,66 @@ import (
 )
 
 func main() {
-	
+
 	exibeIntroducao()
-	exibeMenu()
 
-	comando := leComando()
-	
-	//Scanf é a função para receber dados do usuário 
-	//& - serve para indicar que vai salvar na variável oque o usuário digitar
+	for {
 
+		exibeMenu()
 
-	switch comando {
-	case 1:
-		iniciarMonitoramendo()
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço esse comando...")
-		os.Exit(-1)
+		comando := leComando()
+
+		//Scanf é a função para receber dados do usuário
+		//& - serve para indicar que vai salvar na variável oque o usuário digitar
+
+		switch comando {
+		case 1:
+			iniciarMonitoramendo()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço esse comando...")
+			os.Exit(-1)
+		}
+		//O switch não precisa do "break" em Go
 	}
-	//O switch não precisa do "break" em Go	
-	
+
 }
 
-
-func exibeMenu(){
+func exibeMenu() {
 
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
 }
 
-func exibeIntroducao(){
+func exibeIntroducao() {
 	nome := "Lethicia"
-	versao := 1.1 
+	versao := 1.1
 	fmt.Println("Olá sra", nome)
 	fmt.Println("Este programa está na versão", versao)
 }
 
-func leComando() int{
+func leComando() int {
 	var comandoLido int
-	fmt.Scan(&comandoLido) 
+	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
 
 	return comandoLido
 }
 
-func iniciarMonitoramendo(){
+func iniciarMonitoramendo() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
+	site := "https://random-status-code.hrokuapp.com/"
 	resp, _ := http.Get(site)
 	//fmt.Println(resp)
 
-	if resp.StatusCode == 200{
+	if resp.StatusCode == 200 {
 		fmt.Println("Site: ", site, "foi carregado com sucesso")
-	}else{
+	} else {
 		fmt.Println("Site: ", site, "está com problemas. Status Code: ", resp.StatusCode)
 	}
 }
