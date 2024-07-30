7792a369eec7bd7a0b37b492b245faf80afb06a3
lethiciaasevedo1999 <lethiciaasevedo01@gmail.com>
2024-07-29 22:43:57 -0300

    Ajustando a visualização de logs e seu comportamento no arquivo txt

--- Hello.go
+++ Hello.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"time" //pacote utilizado para o espaçamento de tempo para executar o looping
+	"strconv"
 )
 
 const monitoramentos = 2
@@ -138,12 +139,15 @@ func leSitesDoArquivo() []string {
 }
 
 func registraLog(site string, status bool){
-	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE, 0666)
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE| os.O_APPEND, 0666)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(arquivo)
+	arquivo.WriteString(site + "- online: " + strconv.FormatBool(status) + "\n")
+	
+
+	arquivo.Close() // função para fechar o arquivo após abri-lo
 
 }
